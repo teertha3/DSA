6df1c183260099a4d35d113a942fa7236bd37766
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-11 15:32:49 +0530

    Create leetcode_268.java

---
+++ leetcode_268.java
@@ -0,0 +1,13 @@
+//Solution for Missing number
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int actual_sum=(n*(n+1))/2;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+        }
+        int missing_no=actual_sum-sum;
+        return missing_no;
+    }
+}

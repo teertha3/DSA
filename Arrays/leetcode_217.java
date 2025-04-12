9fb3bd87caa359e5bfa3c649695bdb1b2ca2dd14
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-12 13:45:36 +0530

    Contains Duplicates problem

--- Arrays/leetcode_217.java
+++ Arrays/leetcode_217.java
@@ -12,3 +12,15 @@ class Solution {
     }
 }
 
+//Brute force solution
+public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return true; 
+                }
+            }
+        }
+        return false; 
+    }

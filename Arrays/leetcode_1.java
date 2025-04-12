fc193f507b22e62b25de56e2430849f886bd98fd
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-12 13:53:22 +0530

    Two Sum Problem

--- Arrays/leetcode_1.java
+++ Arrays/leetcode_1.java
@@ -1,4 +1,4 @@
-//Two sum problem
+//Two sum problem brute force solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
@@ -12,3 +12,15 @@ class Solution {
     }
 }
 
+// optimal solution
+public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>(); 
+        for (int i = 0; i < nums.length; i++) {
+            int c= target - nums[i];
+            if (hm.containsKey(c)) {
+                return new int[] { hm.get(c), i };
+            }
+            hm.put(nums[i], i);
+        }
+        return new int[] {}; 
+    }

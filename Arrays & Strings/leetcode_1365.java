05fbd64c257f35929fb883b3d1f7c7cea9a209eb
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-25 11:28:52 +0530

    Update leetcode_1365.java

--- Arrays & Strings/leetcode_1365.java
+++ Arrays & Strings/leetcode_1365.java
@@ -1,4 +1,6 @@
 //How Many Numbers Are Smaller Than the Current Number
+
+//Brute force approach
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int res[]=new int[nums.length];
@@ -13,5 +15,27 @@ class Solution {
         }
         return res;
         
+    }
+}
+
+//count sort approach
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] count = new int[101]; 
+        int[] res = new int[nums.length];
+        for (int num : nums) {
+            count[num]++;
+        }
+        for (int i = 1; i < 101; i++) {
+            count[i] += count[i - 1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                res[i] = 0;
+            else
+                res[i] = count[nums[i] - 1];
+        }
+
+        return res;
     }
 }

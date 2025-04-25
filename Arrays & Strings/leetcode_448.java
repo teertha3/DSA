2b3c0d7de009a70d012c6c73621e5773f36dec14
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-25 11:26:14 +0530

    Find all numbers disappeared problem

--- Arrays & Strings/leetcode_448.java
+++ Arrays & Strings/leetcode_448.java
@@ -1,4 +1,6 @@
 //Find all numbers disappeared
+
+//negative marking approach
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         ArrayList<Integer>li=new ArrayList<>();
@@ -15,5 +17,23 @@ class Solution {
             }
         }
         return li;
+    }
+}
+
+//using hashset
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
     }
 }

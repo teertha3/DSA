22de742157fac337283d23d886af6bf526c13c4c
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-07-22 22:18:42 +0530

    Valid Palindrome (Leetcode 125)

---
+++ Arrays & Strings/ValidPalindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        int l=0;
+        int r=s.length()-1;
+        while(l<=r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;  
+    }
+}

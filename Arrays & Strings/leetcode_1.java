ec650463ec0f6e86abae9335fe4ad558c6503335
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-07-24 21:17:37 +0530

    leetcode_1 (Two sum optimal solution added)

--- Arrays & Strings/leetcode_1.java
+++ Arrays & Strings/leetcode_1.java
@@ -13,14 +13,16 @@ class Solution {
 }
 
 // optimal solution
-public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hm = new HashMap<>(); 
-        for (int i = 0; i < nums.length; i++) {
-            int c= target - nums[i];
-            if (hm.containsKey(c)) {
-                return new int[] { hm.get(c), i };
-            }
-            hm.put(nums[i], i);
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer>hm=new HashMap<>();
+       for(int i=0;i<nums.length;i++){
+        int a=nums[i];
+        if(hm.containsKey(target-a)){
+            return new int[] {hm.get(target-a),i};
         }
-        return new int[] {}; 
+        hm.put(a,i);
+       }
+        return new int[] {};
     }
+}

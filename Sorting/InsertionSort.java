102e993b573cd10d28cd5cf955dce584966b766a
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-08-31 11:55:33 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -13,3 +13,24 @@ class Solution {
         return nums;
     }
 }
+
+//recursion
+class Solution {
+    public int[] insertionSort(int[] nums) {
+        insertion_sort(nums,0,nums.length);
+        return nums;
+    }
+    public static void insertion_sort(int[] nums,int i,int n){
+        if(i==n){
+            return;
+        }
+        int j=i;
+        while(j>0 && nums[j-1]>nums[j]){
+            int temp=nums[j-1];
+            nums[j-1]=nums[j];
+            nums[j]=temp;
+            j--;
+        }
+        insertion_sort(nums,i+1,n);
+    }
+}

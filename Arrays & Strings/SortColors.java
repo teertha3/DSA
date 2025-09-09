8e1b7b4ad821001665389032d30f1c2b84d565b9
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-09 17:11:04 +0530

    Update SortColors.java (written optimal code)

--- Arrays & Strings/SortColors.java
+++ Arrays & Strings/SortColors.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public void sortColors(int[] nums) {
         int cntzero=0;
@@ -27,3 +28,32 @@ class Solution {
         
     }
 }
+
+//Optimal
+class Solution {
+    public void sortColors(int[] nums) {
+       int low=0,mid=0;
+       int high=nums.length-1;
+       while(mid<=high){
+        if(nums[mid]==0){
+            int temp=nums[mid];
+            nums[mid]=nums[low];
+            nums[low]=temp;
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            int temp=nums[mid];
+            nums[mid]=nums[high];
+            nums[high]=temp;
+            high--;
+        }
+       }
+        
+    }
+}
+
+

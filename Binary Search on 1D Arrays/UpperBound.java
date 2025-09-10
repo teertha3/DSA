696f0b0d78a0ad2844c5fd81649b878bff6b89c3
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-10 21:28:52 +0530

    Create UpperBound.java

---
+++ Binary Search on 1D Arrays/UpperBound.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int upperBound(int[] nums, int x) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            if(nums[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}

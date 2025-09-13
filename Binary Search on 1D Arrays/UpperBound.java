f4528fa0410b9f16c230a9ecde4333f4b0464883
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-13 05:46:44 +0530

    Update UpperBound.java

--- Binary Search on 1D Arrays/UpperBound.java
+++ Binary Search on 1D Arrays/UpperBound.java
@@ -17,3 +17,23 @@ class Solution {
         return ans;
     }
 }
+//recursive BS
+class Solution {
+    public int lowerBound(int[] nums, int x) {
+       int n=nums.length;
+       int low=0;
+       int high=n-1;
+       return BS(nums,x,low,high);
+    }
+    public int BS(int[] nums,int x,int low,int high){
+        if(low>high){
+            return nums.length;
+        }
+        int mid=low+((high-low)/2);
+        if(nums[mid]>x){
+            return Math.min(mid,BS(nums,x,low,mid-1));
+        }
+            
+        return BS(nums,x,mid+1,high);
+    }
+}

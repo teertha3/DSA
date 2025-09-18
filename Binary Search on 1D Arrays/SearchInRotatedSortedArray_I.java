74051a6b8cfd4ffbc08d71b3e0828978582cfba4
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-18 23:50:04 +0530

    Update SearchInRotatedSortedArray_I.java

--- Binary Search on 1D Arrays/SearchInRotatedSortedArray_I.java
+++ Binary Search on 1D Arrays/SearchInRotatedSortedArray_I.java
@@ -1,3 +1,16 @@
+//linear search 
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
+                return i; 
+            }
+        }
+        return -1; 
+    }
+}
+
+//binary search
 class Solution {
     public int search(int[] nums, int target) {
         int low=0;

f326f149faf0d971fe3e192b05cbb06b572516a6
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-20 23:55:41 +0530

    Update FindMinInRotatedSortedArray.java

--- Binary Search on 1D Arrays/FindMinInRotatedSortedArray.java
+++ Binary Search on 1D Arrays/FindMinInRotatedSortedArray.java
@@ -5,13 +5,15 @@ class Solution {
         int ans=Integer.MAX_VALUE;
         while(low<=high){
             int mid=low+((high-low)/2);
+            //if left half is sorted
             if(nums[low]<=nums[mid]){
-                ans=Math.min(ans,nums[low]);
-                low=mid+1;
+                ans=Math.min(ans,nums[low]); 
+                low=mid+1; // eliminate left half after taking min from this part
             }
+            //if right half is sorted
             else{
-                ans=Math.min(ans,nums[mid]);
-                high=mid-1;
+                ans=Math.min(ans,nums[mid]); 
+                high=mid-1; //eliminate right half 
             }
         }
         return ans;

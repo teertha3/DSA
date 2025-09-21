fa626a4abe09ba04ff94bf9c2bcecdb92fd20f55
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-09-21 20:36:08 +0530

    Find out how many times the array has been rotated (solution)

---
+++ Binary Search on 1D Arrays/FindNoTimesArrayisRotated.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findKRotation(ArrayList<Integer> nums) {
+        int low=0;
+        int high=nums.size()-1;
+        int ans=Integer.MAX_VALUE;
+        int ind=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            //when array is already sorted
+            if(nums.get(low)<=nums.get(high)){
+                return low;
+            }
+            //if left is sorted
+            if(nums.get(low)<=nums.get(mid)){
+                if(nums.get(low)<ans){
+                    ans=nums.get(low);
+                    ind=low;
+                }
+                low=mid+1; //eliminate left part now
+
+            }
+            //if right is sorted
+            if(nums.get(mid)<=nums.get(high)){
+                if(nums.get(mid)<ans){
+                    ans=nums.get(mid);
+                    ind=mid;
+                }
+                high=mid-1; //eliminate right part now
+            }
+        }
+        return ind;
+
+    }
+}

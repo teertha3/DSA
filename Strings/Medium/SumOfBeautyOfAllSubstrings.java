301d181422b18f3eb3d7a4b2097757b558339748
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-10-07 22:14:02 +0530

    Sum of Beauty of All Substrings (LeetCode 1781)

---
+++ Strings/Medium/SumOfBeautyOfAllSubstrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int beautySum(String s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int freq[]=new int[26];
+            for(int j=i;j<n;j++){
+                freq[s.charAt(j)-'a']++;
+                int max=Integer.MIN_VALUE;
+                int min=Integer.MAX_VALUE;
+                for(int k=0;k<26;k++){
+                    if(freq[k]>0)
+                    min=Math.min(min,freq[k]);
+                    max=Math.max(max,freq[k]);
+                }
+                sum+=max-min;
+            }
+        }
+        return sum;
+    }
+}

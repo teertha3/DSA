b6f44a50593e107eb7490be015cb72257db628bd
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-10-08 21:51:38 +0530

    Roman To Integer (LeetCode 13)

---
+++ Strings/Medium/RomanToInteger.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int romanToInt(String s) {
+        int res=0;
+        HashMap<Character,Integer>hm=new HashMap<>();
+        hm.put('I',1);
+        hm.put('V',5);
+        hm.put('X',10);
+        hm.put('L',50);
+        hm.put('C',100);
+        hm.put('D',500);
+        hm.put('M',1000);
+        for(int i=0;i<s.length()-1;i++){
+            if(hm.get(s.charAt(i))<hm.get(s.charAt(i+1))){
+                res-=hm.get(s.charAt(i));
+            }
+            else{
+                res+=hm.get(s.charAt(i));
+            }
+        }
+        return res+hm.get(s.charAt(s.length()-1));
+    }
+}

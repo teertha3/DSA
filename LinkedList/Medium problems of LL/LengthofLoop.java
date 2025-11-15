24861494cb7bb74d23905d16b6d498503b651239
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-11-15 23:52:29 +0530

    Create LengthofLoop.java

---
+++ LinkedList/Medium problems of LL/LengthofLoop.java
@@ -0,0 +1,39 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+//brute
+ class Solution {
+     public int findLengthOfLoop(ListNode head) {
+        HashMap<ListNode,Integer>hm=new HashMap<>();
+        ListNode temp=head;
+        int t=1;
+        while(temp!=null){
+            if(hm.containsKey(temp)){
+                int v=hm.get(temp);
+                return (t-v);
+            }
+            hm.put(temp,t);
+            t++;
+            temp=temp.next;
+        }
+        return 0;
+     }
+ }

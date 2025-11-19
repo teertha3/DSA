2903022ae3b806e2412016bd3e2cab4d82cf1f8c
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-11-19 23:41:04 +0530

    Update PalindromeLL.java

--- LinkedList/Medium problems of LL/PalindromeLL.java
+++ LinkedList/Medium problems of LL/PalindromeLL.java
@@ -1,4 +1,4 @@
-
+//brute
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if(head==null||head.next==null){
@@ -19,5 +19,48 @@ class Solution {
             temp=temp.next;
         }
         return true;
+    }
+}
+
+//optimal
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null||head.next==null){
+            return true;
+        }
+        //finding middle node
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        //reversing 2nd half
+        ListNode newHead=reverseFun(slow.next);
+        ListNode first=head;
+        ListNode second=newHead;
+        //checking for LL palindrome 
+        while(second!=null){
+            if(first.val!=second.val){
+                newHead=reverseFun(newHead);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+        }
+        newHead=reverseFun(newHead);
+        return true;
+    }
+    public static ListNode reverseFun(ListNode head){
+        ListNode prev_n=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev_n;
+            prev_n=curr;
+            curr=next;
+        }
+        return prev_n;
     }
 }

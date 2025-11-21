c8f25e059aed58bdcd32d9fb8495fec133415d66
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-11-21 22:42:25 +0530

    Create OddEvenLL.java

---
+++ LinkedList/Medium problems of LL/OddEvenLL.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//brute
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ArrayList<Integer>li=new ArrayList<>();
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            li.add(temp.val);
+            temp=temp.next.next;
+            
+        }
+        if(temp!=null){
+            li.add(temp.val);
+        }
+        temp=head.next;
+        while(temp!=null && temp.next!=null){
+            li.add(temp.val);
+            temp=temp.next.next;
+        }
+        if(temp!=null){
+            li.add(temp.val);
+        }
+        temp=head;
+        int i=0;
+        while(temp!=null){
+            temp.val=li.get(i);
+            i++;
+            temp=temp.next;
+        }
+        return head;
+    }
+}

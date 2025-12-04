4e37c836b1ca6068401ea554aa2d359b4f8ec7e9
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-12-04 22:53:48 +0530

    Create DeleteMiddleNodeofLL.java

---
+++ LinkedList/Medium problems of LL/DeleteMiddleNodeofLL.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//Brute
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode temp=head;
+        int c=0;
+        while(temp!=null){
+            c++;
+            temp=temp.next;
+        }
+        if(c==1){
+            return head.next;
+        }
+        int r=c/2;
+        temp=head;
+        while(temp!=null){
+            r--;
+            if(r==0){
+                temp.next=temp.next.next;
+                break;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}

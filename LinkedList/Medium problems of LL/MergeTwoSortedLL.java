b3153a938f27f6f58b8f765e0b6aec86412367a9
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-12-21 23:10:59 +0530

    Update MergeTwoSortedLL.java

--- LinkedList/Medium problems of LL/MergeTwoSortedLL.java
+++ LinkedList/Medium problems of LL/MergeTwoSortedLL.java
@@ -37,3 +37,64 @@ class Solution {
        
     }
 }
+
+
+//Optimal
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+       if(head==null || head.next==null){
+        return head;
+       }
+       ListNode mid=findMid(head);
+       ListNode lefthead=head;
+       ListNode righthead=mid.next;
+       mid.next=null;
+       lefthead=sortList(lefthead);
+       righthead=sortList(righthead);
+       return mergeLL(lefthead,righthead);
+    }
+    public static ListNode findMid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode mergeLL(ListNode lefthead,ListNode righthead){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(lefthead!=null && righthead!=null){
+            if(lefthead.val<righthead.val){
+                temp.next=lefthead;
+                temp=lefthead;
+                lefthead=lefthead.next;
+            }
+            else{
+                temp.next=righthead;
+                temp=righthead;
+                righthead=righthead.next;
+            }
+        }
+        if(lefthead!=null){
+            temp.next=lefthead;
+        }
+        else{
+            temp.next=righthead;
+        }
+        return dummy.next;
+
+    }
+    
+}

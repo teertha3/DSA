b001d1c8d383bb6853f207deca1b0605879ea082
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-12-24 22:33:43 +0530

    Update SortLLof0s1s2s.java (Optimal solution)

--- LinkedList/Medium problems of LL/SortLLof0s1s2s.java
+++ LinkedList/Medium problems of LL/SortLLof0s1s2s.java
@@ -44,3 +44,58 @@ class Solution {
         return head;
     }
 }
+
+//optimal 
+/*
+Definition of singly linked list:
+class ListNode{
+    public int data;
+    public ListNode next;
+    ListNode() { data = 0; next = null; }
+    ListNode(int x) { data = x; next = null; }
+    ListNode(int x, ListNode next) { data = x; this.next = next; }
+}
+*/
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode zeroHead=new ListNode(-1);
+        ListNode oneHead=new ListNode(-1);
+        ListNode twoHead=new ListNode(-1);
+        ListNode zero=zeroHead;
+        ListNode one=oneHead;
+        ListNode two=twoHead;
+        ListNode temp=head;
+        while(temp!=null){
+            ListNode nextNode = temp.next;
+            temp.next = null;   
+            if(temp.data==0){
+                zero.next=temp;
+                zero=temp;
+            }
+            else if(temp.data==1){
+                one.next=temp;
+                one=temp;
+            }
+            else{
+                two.next=temp;
+                two=temp;
+            }
+            temp=nextNode;
+        }
+        if(oneHead.next!=null){
+            zero.next=oneHead.next;
+        }
+        else{
+            zero.next=twoHead.next;
+        }
+        if(twoHead.next!=null){
+            one.next=twoHead.next;
+        }
+        two.next=null;
+        return zeroHead.next;
+    }
+}

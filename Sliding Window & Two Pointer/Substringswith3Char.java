afba41d11cba16022585fb5f4b88aae026ac3ce8
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2026-01-16 14:10:38 +0530

    No.of substrings with all 3 characters (Optimal solution)

--- Sliding Window & Two Pointer/Substringswith3Char.java
+++ Sliding Window & Two Pointer/Substringswith3Char.java
@@ -14,3 +14,17 @@ class Solution {
         return c;
     }
 }
+//optimal
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int c=0;
+        int lastseen[]={-1,-1,-1};
+        for(int i=0;i<s.length();i++){
+            lastseen[s.charAt(i)-'a']=i;
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+                c+=(1+Math.min(Math.min(lastseen[0],lastseen[1]),lastseen[2]));
+            }
+        }
+        return c;
+    }
+}

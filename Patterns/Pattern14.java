ba94beeb51d6ae84c91975d7d8b6cedbbe8ddc9f
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2026-05-12 16:53:15 +0530

    Update Pattern14.java

--- Patterns/Pattern14.java
+++ Patterns/Pattern14.java
@@ -1,9 +1,9 @@
 /*Let's say for N = 5, the pattern should look like as below:
-1 
-2 3 
-4 5 6 
-7 8 9 10 
-11 12 13 14 15
+A
+AB
+ABC
+ABCD
+ABCDE
 */
 class Solution {
     public void pattern14(int n){
